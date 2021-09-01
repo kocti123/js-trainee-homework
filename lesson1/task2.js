8199652186627ebc8a29ee586162506bce1797ee
Konstantin Batuev <batyev.kv@gmail.com>
2021-09-01 21:56:53 +0300

    in function sumAndDivide text in prompt changed to russia

--- lesson1/task2.js
+++ lesson1/task2.js
@@ -1,11 +1,11 @@
 function sumAndDivide() {
-  const num1 = +prompt('Enter first number');
+  const num1 = +prompt('Введите первое число');
   if (Number.isNaN(num1)) {
     console.log('Некорректный ввод!');
     return;
   }
 
-  const num2 = +prompt('Enter secong number');
+  const num2 = +prompt('Введите второе число');
   if (Number.isNaN(num2) || num2 === 0) {
     console.log('Некорректный ввод!');
     return;
