c9a0930f548e5f839aa5da59f2e7efc9194e2cfd
Konstantin Batuev <batyev.kv@gmail.com>
2021-09-04 18:19:04 +0300

    add function selectFromInterval

--- lesson2/lesson2.js
+++ lesson2/lesson2.js
@@ -11,3 +11,29 @@ function makeObjectDeepCopy(objToCopy) {
   return objCopy;
 }
 
+function selectFromInterval(arr, from, to) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError('first arguments must be array')
+  }
+  for (const number of arr) {
+    if (typeof number !== 'number') {
+      throw new TypeError('all values in first argument must be numbers');
+    }
+  }
+  if (!Number.isInteger(from) || !Number.isInteger(to)) {
+    throw new TypeError('second and third arguments must be integers')
+  }
+
+  if (to < from) {
+    console.log('swap');
+    [to, from] = [from, to];
+  }
+
+  const newArr = [];
+  for (let i = from - 1; i < to; i++) {
+    if (typeof arr[i] !== 'number') continue;
+    newArr.push(arr[i])
+  }
+  return newArr;
+}
+
