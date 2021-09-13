b4fd7b25377983d6f2fdce24444969243236dd1a
Konstantin Batuev <batyev.kv@gmail.com>
2021-09-13 11:48:59 +0300

    concatenate strings instead arrays

--- lesson4/lesson4.js
+++ lesson4/lesson4.js
@@ -1,9 +1,9 @@
 function concatStrings(str, separator) {
-  let result = [str];
+  let result = str;
   const sep = typeof separator === 'string' ? separator : '';
   return function concat(str) {
-    if (typeof str !== 'string') return result.join(sep);
-    result = [...result, str];
+    if (typeof str !== 'string') return result;
+    result = result + sep + str;
     return concat;
   };
 }
