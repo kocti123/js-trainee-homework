434b0f35ec010cd660439b1bd739fe892d188102
Konstantin Batuev <batyev.kv@gmail.com>
2021-09-13 11:51:16 +0300

    Calculator rewrited using class syntax

--- lesson4/lesson4.js
+++ lesson4/lesson4.js
@@ -8,47 +8,42 @@ function concatStrings(str, separator) {
   };
 }
 
-function Calculator(x, y) {
-  if (!Number.isFinite(x) || !Number.isFinite(y)) {
-    throw new Error('should be finite number');
-  }
-
-  this.x = x;
-  this.y = y;
-
-  this.setX = function (x) {
-    if (!Number.isFinite(x)) throw new Error('');
+class Calculator {
+  constructor(x, y) {
+    if (!Number.isFinite(x) || !Number.isFinite(y)) {
+      throw new Error('should be finite number');
+    }
+    
     this.x = x;
-  };
-
-  this.setY = function (y) {
-    if (!Number.isFinite(y)) throw new Error('');
     this.y = y;
-  };
-
-  this.logSum = function () {
-    console.log(this.x + this.y);
-  };
-
-  this.logMul = function () {
-    console.log(this.x * this.y);
-  };
-
-  this.logSub = function () {
-    console.log(this.x - this.y);
-  };
-
-  this.logDiv = function () {
-    if (this.y === 0) throw new Error('');
-    console.log(this.x / this.y);
-  };
-
-  this.setX = this.setX.bind(this);
-  this.setY = this.setY.bind(this);
-  this.logSum = this.logSum.bind(this);
-  this.logMul = this.logMul.bind(this);
-  this.logSub = this.logSub.bind(this);
-  this.logDiv = this.logDiv.bind(this);
+    
+    this.setX = function (x) {
+      if (!Number.isFinite(x)) throw new Error('');
+      this.x = x;
+    }.bind(this);
+    
+    this.setY = function (y) {
+      if (!Number.isFinite(y)) throw new Error('');
+      this.y = y;
+    }.bind(this);
+    
+    this.logSum = function () {
+      console.log(this.x + this.y);
+    }.bind(this);
+    
+    this.logMul = function () {
+      console.log(this.x * this.y);
+    }.bind(this);
+    
+    this.logSub = function () {
+      console.log(this.x - this.y);
+    }.bind(this);
+    
+    this.logDiv = function () {
+      if (this.y === 0) throw new Error('');
+      console.log(this.x / this.y);
+    }.bind(this);
+  }
 }
 
 export { concatStrings, Calculator };
